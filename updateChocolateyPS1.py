91f623516c5c265c8fa8d4c9a2e428eefb06fb05
Varun V Deshpande <varun.vd51@gmail.com>
2023-01-24 22:06:19 +0530

    Update updateChocolateyPS1.py

--- updateChocolateyPS1.py
+++ updateChocolateyPS1.py
@@ -8,16 +8,16 @@ def main(url, checksum, url64, checksum64):
             if len(splitLine) < 1:
                 continue
             if splitLine[0] == '$url' and splitLine[1] == "=":
-                splitLine[2] = f"'{url}'"
+                splitLine[2] = f"'{url.strip()}'"
                 line[index] = ' '.join(splitLine) + '\n'
             if splitLine[0] == '$url64' and splitLine[1] == "=":
-                splitLine[2] = f"'{url64}'"
+                splitLine[2] = f"'{url64.strip()}'"
                 line[index] = ' '.join(splitLine) + '\n'
             if splitLine[0] == 'checksum' and splitLine[1] == "=":
-                splitLine[2] = f"'{checksum}'"
+                splitLine[2] = f"'{checksum.strip()}'"
                 line[index] = ' '.join(splitLine) + '\n'
             if splitLine[0] == 'checksum64' and splitLine[1] == "=":
-                splitLine[2] = f"'{checksum64}'"
+                splitLine[2] = f"'{checksum64.strip()}'"
                 line[index] = ' '.join(splitLine) + '\n'
     with open('tools/chocolateyinstall.ps1', 'w') as ps1:        
         ps1.writelines(line)
